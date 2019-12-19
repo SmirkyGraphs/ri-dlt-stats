503238bd9998a7ce727b2c3628659b9203e3ff34
Alex <SmirkyGraphs@gmail.com>
2019-12-19 07:41:36 -0500

    Update scraper.py
    - add wait for file to finish downloading before moving.
    - moved final scraping functions over from main.py
    - added statewide employment stats scraper

--- src/scraper.py
+++ src/scraper.py
@@ -1,7 +1,8 @@
 import os
-import shutil
 import time
+import shutil
 
+# scrape years and only most recent with --update
 def scrape_years(browser, url, xpath, update):
     # get years from dropdown options
     browser.get(url)
@@ -16,8 +17,20 @@ def scrape_years(browser, url, xpath, update):
 # create function to move file after downloading
 def move_file(year, file_path):
     temp_path = './data/temp/'
+    
+    while not os.listdir(temp_path):
+        time.sleep(3)
+
     temp_file = temp_path + os.listdir(temp_path)[0]
     stem_file = temp_file.split('.')[-1]
+    
+    # check if file finished downloading
+    while stem_file == 'tmp':
+        time.sleep(3)
+        print('[status] waiting on file to download...')
+        temp_file = temp_path + os.listdir(temp_path)[0]
+        stem_file = temp_file.split('.')[-1]
+
     new_file = f'./data/raw/{file_path}/{year}.{stem_file}'
     shutil.move(temp_file, new_file)
 
@@ -27,6 +40,7 @@ def download_file(browser, xpath, file_path, year):
         time.sleep(2)
         move_file(year, file_path)
 
+############################ Generalized Scrapers ############################
 # download ces stats
 def download_ces(browser, report_type, xpath, years):
     url = f'http://www.dlt.ri.gov/lmi/ces/{report_type}'
@@ -60,6 +74,7 @@ def download_ui(browser, xpath, years):
         # download excel file
         download_file(browser, xpath, file_path, year)
 
+# download supply & demand
 def download_sd(browser, years):
     url = 'http://www.dlt.ri.gov/lmi/excel'
     file_path = 'supply_demand'
@@ -75,6 +90,7 @@ def download_sd(browser, years):
         # move downloaded file
         move_file(fname, file_path)
 
+# download industry projections
 def download_industry_proj(browser, xpath, report_type):
     url = f'http://www.dlt.ri.gov/lmi/proj/{report_type}indproj.htm'
     file_path = 'projections/industry'
@@ -85,6 +101,7 @@ def download_industry_proj(browser, xpath, report_type):
     # download excel file
     download_file(browser, xpath, file_path, report_type)
 
+# download occupation projections
 def download_occupation_proj(browser, xpath, report_type):
     url = 'http://www.dlt.ri.gov/lmi/proj'
     file_path = 'projections/occupation'
@@ -100,6 +117,7 @@ def download_occupation_proj(browser, xpath, report_type):
     # download excel file
     download_file(browser, xpath, file_path, report_type)
 
+# download new_england employment
 def download_new_england(browser, xpath, report_type):
     url = 'http://www.dlt.ri.gov/lmi/laus/us'
     file_path = 'geographic/new_england'
@@ -115,6 +133,7 @@ def download_new_england(browser, xpath, report_type):
     # download excel file
     download_file(browser, xpath, file_path, report_type)
 
+# download city/town employment
 def download_citytown(browser, url, xpath, download):
     # get every city/town
     browser.get(url)
@@ -136,6 +155,23 @@ def download_citytown(browser, url, xpath, download):
         count += 1
         time.sleep(3)
 
+# download statewide employment
+def download_statewide(browser, xpath, report_type):
+    url = 'http://www.dlt.ri.gov/lmi/laus/state'
+    file_path = 'geographic/state'
+
+    if report_type == 'seasonal_adjusted':
+        url = f'{url}/seas.htm'
+    else:
+        url = f'{url}/unadj.htm'
+
+    # request url
+    browser.get(url)
+
+    # download excel file
+    download_file(browser, xpath, file_path, report_type)
+
+# download wage data
 def download_wages(browser, xpath, report_type):
     file_path = 'wages'
 
@@ -151,3 +187,87 @@ def download_wages(browser, xpath, report_type):
 
     # download excel file
     download_file(browser, xpath, file_path, report_type)
+
+############################ Start of Scrapers ############################
+def scrape_current_employment(browser, update):
+    # get years and download button for ces stats
+    url = 'http://www.dlt.ri.gov/lmi/ces.htm'
+    button_options = '//*[@id="form"]/font/select/option'
+    years = scrape_years(browser, url, button_options, update)
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b'
+
+    # scrape sesoanlly adjusted ces stats
+    adjusted_download = f'{download}/span/font/a[1]'
+    download_ces(browser, 'seasonal', adjusted_download, years)
+
+    # scrape non-adjusted ces stats
+    non_adj_download = f'{download}/font[2]/font[2]/strong/a[1]'
+    download_ces(browser, 'state', non_adj_download, years)
+
+def scrape_ui_claims(browser, update):
+    # get years and download button for ui claims
+    url = 'http://www.dlt.ri.gov/lmi/uiadmin.htm'
+    button_options = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[1]/td[2]/table/tbody/tr[7]/td/p/font/select/option'
+    years = scrape_years(browser, url, button_options, update)
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[1]/td/p[1]/b/font/a[1]'
+
+    # scrape ui claims
+    download_ui(browser, download, years)
+
+def scrape_supply_demand(browser, update):
+    # get quarter-year for supply & demand
+    url = 'http://www.dlt.ri.gov/lmi/publications/supply&demand.htm'
+    button_options = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[2]/td/blockquote/table/tbody/tr[2]/td[3]/select/option'
+    years = scrape_years(browser, url, button_options, update)
+
+    # scrape supply & demand
+    download_sd(browser, years)
+
+def scrape_projections(browser, update):
+    if update == True:
+        return
+
+    # download major industry 
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[2]/font/a[1]'
+    download_industry_proj(browser, download, 'major')
+
+    # download 3 digit naics industry
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[3]/font/a[1]'
+    download_industry_proj(browser, download, 'naics3')
+
+    # download specific occupations (uses same download xpath as above)
+    download_occupation_proj(browser, download, 'occupations')
+
+    # download major occupation groups (uses same download xpath as above)
+    download_occupation_proj(browser, download, 'major_occ_group')
+
+def scrape_geographic(browser):
+    # download statewide unemployment
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[2]/a[1]'
+    download_statewide(browser, download, 'non_adjusted')
+    download_statewide(browser, download, 'seasonal_adjusted')
+
+    # download new england unemployment
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[2]/font[2]/a[1]'
+    download_new_england(browser, download, 'non_adjusted')
+    download_new_england(browser, download, 'seasonal_adjusted')
+
+    # get employment by city/town
+    url = 'http://www.dlt.ri.gov/lmi/laus/town/town.htm'
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr/td/p/b/span/font/a' 
+    button_options = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/form/table/tbody/tr[4]/td[2]/select/option'
+    download_citytown(browser, url, button_options, download)
+
+def scrape_wages(browser, update):
+    if update == True:
+        return
+
+    # download wage information
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[2]/font/a'
+    download_wages(browser, download, 'total_wages')
+
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[2]/font[2]/a'
+    download_wages(browser, download, 'occupation_wages')
+
+    download = '/html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table[1]/tbody/tr[1]/td/b/font[3]/font/a'
+    download_wages(browser, download, 'new_england_wages')
